745d401977b1db9b646be16a61cf4b9a3d3a3f24
pinterkarimandras <pinterkarimandras@81f6dc66-1995-11df-bff1-5f4ea15f2b7c>
2010-03-12 14:40:16 +0000

    Fixing some compilation problems on win.

--- fmlistview.cpp
+++ fmlistview.cpp
@@ -18,12 +18,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "fmlistview.h"
-#include "fmfilesystemmodel.h"
 #include <QDebug>
 #include <QShortcut>
 #include <QUrl>
 #include <QHeaderView>
 #include <QStandardItemModel>
+#include <QDir>
 
 FMListView::FMListView( QWidget *parent) :
     QListView(parent), fileList(NULL)
--- fmpanel.h
+++ fmpanel.h
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include "bookmarklistview.h"
 #include "historylistview.h"
 #include "drivelistview.h"
-#include "fmfilesystemmodel.h"
 #include <QObject>
 #include <QWidget>
 #include <QHBoxLayout>
