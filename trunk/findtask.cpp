c95f313918e6e7b67096811f90611f6be008a7f8
pinterkarimandras <pinterkarimandras@81f6dc66-1995-11df-bff1-5f4ea15f2b7c>
2010-05-26 19:37:35 +0000

    OSX find is on the way but not working yet.

--- trunk/findtask.cpp
+++ trunk/findtask.cpp
@@ -3,12 +3,23 @@
 
 findTask::findTask(const QString& dirName, const QString& pattern)
 {
+#ifdef Q_WS_WIN
     args<<"/c"<<"dir"<<"/b"<<"/s"<<pattern;
-    //qDebug()<<args;
+#elif defined(Q_WS_MAC)
+    //find /Users/karimpinter/ -name "*.txt" -print
+    QString pat;
+    pat.append("\"");
+    pat.append(pattern);
+    pat.append("\"");
+    args<<dirName<<"-name"<<pat<<"-print";
+#elif defined(Q_OS_LINUX)
+#endif
+    qDebug()<<args;
     process = new QProcess();
     process->setReadChannel( QProcess::StandardOutput );
     process->setReadChannelMode( QProcess::SeparateChannels );
     process->setWorkingDirectory( dirName );
+    //process->setWorkingDirectory( "/usr/bin" );
     //connect( process, SIGNAL(finished( int , QProcess::ExitStatus )), this, SLOT( findTaskFinished( int , QProcess::ExitStatus ) ));
     connect( process, SIGNAL(readyRead ()), this, SLOT( readFindResult() ));
 }
@@ -20,7 +31,18 @@ findTask::~findTask()
 
 void findTask::run()
 {
+#ifdef Q_WS_WIN
     process->start("cmd.exe", args);
+#elif defined(Q_WS_MAC)
+    process->start("find", args);
+    if (!process->waitForStarted())
+        return;
+
+    if (!process->waitForFinished())
+        return;
+    qDebug()<<process->readLine();
+#elif defined(Q_OS_LINUX)
+#endif
 }
 
 void findTask::readFindResult()
@@ -33,6 +55,7 @@ void findTask::readFindResult()
     {
         list[j].chop(1);
     }
+    qDebug()<<list;
     emit getSearchResult( list );
     return;
 }
